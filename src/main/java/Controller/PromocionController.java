2986f17ff501b6e3e6f4398eea4b0f67707869aa
Pablo.ED <echiparradominguezp@gmail.com>
2025-09-29 02:04:36 -0500

    Enpoints Sphannajer

---
+++ src/main/java/Controller/PromocionController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import Service.PromocionService;
+import dto.PromocionesDTO;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/promociones")
+public class PromocionController {
+
+    private final PromocionService service;
+
+    public PromocionController(PromocionService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<PromocionesDTO> listar() {
+        return service.listarPromociones();
+    }
+
+    @PostMapping
+    public PromocionesDTO guardar(@RequestBody PromocionesDTO dto) {
+        return service.guardarPromocion(dto);
+    }
+}
