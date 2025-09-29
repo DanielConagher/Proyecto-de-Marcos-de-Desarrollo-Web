9072ab2fa677c50da11f49308fbef4b0960fe7b6
DemonALIST <frankvidal06@gmail.com>
2025-09-28 22:57:47 -0500

    Se añade la estructura del proyecto. Los endpoints, entities,

---
+++ src/main/java/Entity/Extra.java
@@ -0,0 +1,59 @@
+package Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "Extra")
+public class Extra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_extra;
+    
+    private String nombre;
+    private BigDecimal precio;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_id") // El nombre de la columna foránea ahora apunta a 'menu'
+    private Menu menu; // El nombre de la variable debe ser 'menu' para que coincida con 'mappedBy'
+
+    // Getters y Setters
+    public long getId_extra() {
+        return id_extra;
+    }
+
+    public void setId_extra(long id_extra) {
+        this.id_extra = id_extra;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public void setMenu(Menu menu) {
+        this.menu = menu;
+    }
+}
---
+++ src/main/java/Entity/Menu.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.MapKeyColumn;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.List;
+
+/**
+ *
+ * @author danie
+ */
+@Entity
+@Table(name = "Menu") // Nombre de la tabla en la base de datos
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_Menu; // Cambia el nombre del ID
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(length = 255)
+    private String descripcion;
+
+    @ElementCollection
+    @CollectionTable(name = "Menu_Precios", joinColumns = @JoinColumn(name = "menu_id"))
+    @MapKeyColumn(name = "tamano")
+    @Column(name = "precio")
+    private Map<String, BigDecimal> preciosPorTamano;
+
+    @OneToMany(mappedBy = "menu") // El atributo 'mappedBy' debe coincidir con el nombre de la variable en la clase 'Extra'
+    private List<Extra> extras;
+
+    // Getters y Setters
+    public long getId_Menu() {
+        return id_Menu;
+    }
+
+    public void setId_Menu(long id_Menu) {
+        this.id_Menu = id_Menu;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Map<String, BigDecimal> getPreciosPorTamano() {
+        return preciosPorTamano;
+    }
+
+    public void setPreciosPorTamano(Map<String, BigDecimal> preciosPorTamano) {
+        this.preciosPorTamano = preciosPorTamano;
+    }
+
+    public List<Extra> getExtras() {
+        return extras;
+    }
+
+    public void setExtras(List<Extra> extras) {
+        this.extras = extras;
+    }
+}
